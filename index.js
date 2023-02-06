0bd1489d9ec7e20d499df6e7a9eb934b05374dd2
N3mEee <n3mebusiness@gmail.com>
2023-02-06 23:40:37 +0100

    Add temp crateGameBoard function

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // TODO:
 // Put together a script that creates a game board and a knight.
+function createGameBoard() {
+    let board = [];
+    for (let i = 0; i < 8; i++) {
+        board[i] = [];
+        for (let j = 1; j <= 8; j++) {
+            board[i].push(`${String.fromCharCode(i + 97)}${j}`);
+        }
+    }
+    return board.reverse();
+}
+console.log(createGameBoard());
 // Treat all possible moves the knight could make as children in a tree. Don’t allow any moves to go off the board.
 // Decide which search algorithm is best to use for this case. Hint: one of them could be a potentially infinite series.
 // Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square.
